c1010ed365310d0d480fa174ec8aebd56fff7442
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-18 11:24:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,10 +19,8 @@ def take_a_number(katz_deli, name)
 end 
 
 def now_serving(katz_deli)
-  if katz_deli.length > 0 
-    puts 
-    
-    katz_deli.length == 0 
+  if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
+  else puts "Currently serving "
 end 
 end 
