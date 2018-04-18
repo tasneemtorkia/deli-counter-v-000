c9c8863f375ecf77be7122d16eb93a5091c48de7
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-18 11:24:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,6 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
-  else puts "Currently serving "
+  else puts "Currently serving #{katz_deli.shift}."
 end 
 end 
