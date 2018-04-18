00798f1b706bd308afe5bb603315a62788b08e69
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-18 11:15:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. you are number #{katz_deli.length + 1} in line."
+  puts "Welcome, #{name}. you are number #{katz_deli.length} in line."
 end 
