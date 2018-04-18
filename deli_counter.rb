8fa5fee7b2440490e044fb85c862f664d8e4b804
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-18 11:08:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-     katz_deli.collect.each_with_index(1) do |katz_deli, index| 
-     queue.push("#{index}. #{array}")
+     katz_deli.collect.each_with_index do |katz_deli, index| 
+     queue.push("#{index + 1}. #{array}")
   end 
   puts "The line is currently #{queue.join(" ")}"
 end
